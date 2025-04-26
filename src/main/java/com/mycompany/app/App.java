48c53d4edfd799f14ac67d4d42bc3b1086094bf3
Alison <hoalison0309@gmail.com>
2025-04-26 15:52:23 +0800

    Update

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -2,27 +2,18 @@ package com.mycompany.app;
 
 import static spark.Spark.get;
 import static spark.Spark.port;
-import static spark.Spark.post;
 
 public class App {
-    private static final String MESSAGE = "Hello from Spark Server!";
+
+    private static final String MESSAGE = "Hello from Spark Web Server!";
 
     public App() {}
 
     public static void main(String[] args) {
-        port(8082);
-
+        port(8082);  // Ensures the app is listening on port 8081
         get("/", (req, res) -> MESSAGE);
-
-        post("/github-webhook/", (req, res) -> {
-            System.out.println("Webhook received!");
-            System.out.println("Payload: " + req.body());
-            res.status(200);
-            return "Webhook received!";
-        });
     }
 
-    // ADD THIS BACK
     public String getMessage() {
         return MESSAGE;
     }
